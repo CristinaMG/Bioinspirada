0f442cefc4f63fd39fdd6ea090c888f0a7d02bc6
CristinaMG <crismengu@gmail.com>
2018-01-21 20:17:36 +0100

    En proceso

--- MyWindow.py
+++ MyWindow.py
@@ -13,47 +13,27 @@ import matplotlib.pyplot as plt
 import random
 import numpy as np
 from PyQt4.QtGui import QDoubleValidator, QIntValidator
+from time import sleep
 
 # Cargar nuestro archivo .ui
 form_class = uic.loadUiType("camerasWindow.ui")[0]
 
-# Influencia de las feromonas
-influenciaFer = 0.5
-# Influencia de la distancia respecto al espacio cubierto
-pesoFitness = 0.0
-# coeficiente de evaporación de las feromonas
-evaporacion = 0.9  # Se evapora un 10%
-# Umbral que aceptamos para obtener una solución
-umbral = 0.2
-# Número de hormigas locas
-numLocas = 20  # 5% de las hormigas son locas
-# Número de hormigas para el algoritmo
-numHormigas = 5
-# Número de cámaras por cada space
-numCamaras = 6
-# Espacio que abarca cada cámara
-focus = 35
-# Espacio máximo que abarca cada cámara
-distMax = 2 * focus
-# Filas
-rows = 50
 # Columnas
-cols = 2 * rows
+cols = 100
 
 # Class to draw the space and the cameras position
-
-
 class MyCanvasSpace(FigureCanvas):
     def __init__(self, *args, **kwargs):
         self.space = args[1]
 
-        self.fig = Figure(figsize=(kwargs.pop('width'),
-                                   kwargs.pop('height')), dpi=kwargs.pop('dpi'))
+        self.fig = Figure(figsize=(kwargs.get('width'),
+                                   kwargs.get('height')), dpi=kwargs.pop('dpi'))
         self.axes = self.fig.add_subplot(111)
         #self.fig1, self.axes = plt.subplots()
-        self.axes.set_xlim((0, cols))
-        self.axes.set_ylim((0, rows))
+        self.axes.set_xlim((0, kwargs.get('height')))
+        self.axes.set_ylim((0, kwargs.get('width')))
 
+        self.focus = kwargs.get('focus')
         self.init_figure()
         FigureCanvas.__init__(self, self.fig)
         self.setParent(kwargs.pop('parent'))
@@ -67,44 +47,42 @@ class MyCanvasSpace(FigureCanvas):
         timer.timeout.connect(self.update_figure())
         timer.start(1000)'''
 
+    def init_figure(self):
+        plt.ion()
+        #i = 1
+        for cam in range(len(self.space.cameras)):
+            self.axes.add_artist(plt.Circle((self.space.cameras[cam][1]* self.space.height / cols, self.space.cameras[cam][0]),
+                                            self.focus, color=self.space.color[cam], fill=self.space.color[cam], alpha=0.5, clip_on=True))
+            self.axes.annotate("C" + str(cam),
+                               [self.space.cameras[cam][1]* self.space.height / cols, self.space.cameras[cam][0]])
+            self.axes.plot((self.space.cameras[cam][1]* self.space.height / cols), (self.space.cameras[cam]
+                                                          [0]), 'o', color=self.space.color[cam])
+            #i += 1
+        self.fig.gca().set_aspect('equal', 'box')
+
     def update_figure(self):
         #global fig, ax
         self.axes.cla()
-        i = 1
+        #i = 1
         for cam in range(len(self.space.cameras)):
-            self.axes.add_artist(plt.Circle((self.space.cameras[cam][1], self.space.cameras[cam][0]),
-                                            focus, color=self.space.color[cam], fill=self.space.color[cam], alpha=0.5, clip_on=True))
+            self.axes.add_artist(plt.Circle((self.space.cameras[cam][1]* self.space.height / cols, self.space.cameras[cam][0]),
+                                            self.focus, color=self.space.color[cam], fill=self.space.color[cam], alpha=0.5, clip_on=True))
             self.axes.annotate("C" + str(cam),
-                               [self.space.cameras[cam][1], self.space.cameras[cam][0]])
-            self.axes.plot((self.space.cameras[cam][1]), (self.space.cameras[cam]
+                               [self.space.cameras[cam][1]* self.space.height / cols, self.space.cameras[cam][0]])
+            self.axes.plot((self.space.cameras[cam][1]* self.space.height / cols), (self.space.cameras[cam]
                                                           [0]), 'o', color=self.space.color[cam])
-            i += 1
+            #i += 1
 
         self.fig.gca().set_aspect('equal', 'box')
-        plt.show()
-        plt.pause(5)
+        #plt.show()
+        #plt.pause(5)
         # self.axes.clear()
 
-    def init_figure(self):
-        plt.ion()
-        i = 1
-        for cam in range(len(self.space.cameras)):
-            self.axes.add_artist(plt.Circle((self.space.cameras[cam][1], self.space.cameras[cam][0]),
-                                            focus, color=self.space.color[cam], fill=self.space.color[cam], alpha=0.5, clip_on=True))
-            self.axes.annotate("C" + str(cam),
-                               [self.space.cameras[cam][1], self.space.cameras[cam][0]])
-            self.axes.plot((self.space.cameras[cam][1]), (self.space.cameras[cam]
-                                                          [0]), 'o', color=self.space.color[cam])
-            i += 1
-        self.fig.gca().set_aspect('equal', 'box')
-
 # Class to draw the space and the cameras position
-
-
 class MyCanvasPheromone(FigureCanvas):
     def __init__(self, *args, **kwargs):
         self.space = args[1]
-
+        self.cols = kwargs.get('height')
         self.fig = Figure(figsize=(kwargs.get('width'),
                                    kwargs.get('height')), dpi=kwargs.get('dpi'))
         self.axes = self.fig.add_subplot(211)
@@ -126,35 +104,6 @@ class MyCanvasPheromone(FigureCanvas):
         timer.timeout.connect(self.update_figure())
         timer.start(1000)'''
 
-    def update_figure(self):
-        vcols = np.zeros(cols)
-        for i in range(cols):
-            vcols[i] = i
-
-        self.axes.cla()
-
-        plt.subplot(211)
-        plt.cla()
-        plt.title('Histogram of pheromones of up wall')
-        plt.xlabel('Point on wall')
-        plt.ylabel('Pheromone')
-        plt.axis([0, cols, 0, 50])
-        plt.bar(vcols, self.space.wall2,
-                facecolor='#9999ff', edgecolor='white')
-
-        self.axes2.cla()
-
-        plt.subplot(212)
-        plt.cla()
-        plt.title('Histogram of pheromones of down wall')
-        plt.xlabel('Point on wall')
-        plt.ylabel('Pheromone')
-        plt.axis([0, cols, 0, 30])
-        plt.bar(vcols, self.space.wall1,
-                facecolor='#1c702d', edgecolor='white')
-
-        plt.subplots_adjust(hspace=0.5)
-        plt.show()
 
     def init_figure(self):
         vcols = np.zeros(cols)
@@ -180,7 +129,33 @@ class MyCanvasPheromone(FigureCanvas):
         self.fig.subplots_adjust(hspace=0.9)
 
 
-class MyWindow(QtGui.QMainWindow, form_class):
+    def update_figure(self):
+        vcols = np.zeros(cols)
+        for i in range(cols):
+            vcols[i] = i
+
+        self.axes.cla()
+        self.axes.set_title('Histograma de feromonas pared superior')
+        self.axes.set_xlabel('Punto en la pared')
+        self.axes.set_ylabel('Feromona')
+        self.axes.axis([0, cols, 0, 50])
+        self.axes.bar(vcols, self.space.wall2,
+                      facecolor='#9999ff', edgecolor='white')
+
+        self.axes2.cla()
+        self.axes2.set_title('Histograma de feromonas pared inferior')
+        self.axes2.set_xlabel('Punto en la pared')
+        self.axes2.set_ylabel('Feromona')
+        self.axes2.axis([0, cols, 0, 50])
+        self.axes2.bar(vcols, self.space.wall1,
+                       facecolor='#1c702d', edgecolor='white')
+
+        self.fig.subplots_adjust(hspace=0.9)
+
+
+class MyWindow(QtGui.QMainWindow, form_class,QObject):
+
+    draw = pyqtSignal()
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
         self.setupUi(self)
@@ -192,10 +167,10 @@ class MyWindow(QtGui.QMainWindow, form_class):
         self.numCamerasF = 6
         self.distPowerF = 0.5
         self.pheromPowerF = 0.5
-        self.evaporationF = 0.9
+        self.evaporationF = 0.3
         self.numAntsF = 5
-        self.numCrazyF = 20.0
-        self.minThresholdF = 0.2
+        self.numCrazyF = 5.0
+        self.minThresholdF = 0.1
 
         onlyFloat = QDoubleValidator()
         onlyFloat.setNotation(QDoubleValidator.StandardNotation)
@@ -219,16 +194,16 @@ class MyWindow(QtGui.QMainWindow, form_class):
         self.pheromPower.setText('0.5')
         self.pheromPower.setValidator(onlyFloat)
         self.pheromPower.textEdited.connect(self.getPheromPower)
-        self.evaporation.setText('0.9')
+        self.evaporation.setText('0.3')
         self.evaporation.setValidator(onlyFloat)
         self.evaporation.textEdited.connect(self.getEvaporation)
         self.numAnts.setText('5')
         self.numAnts.setValidator(onlyInt)
         self.numAnts.textEdited.connect(self.getAnts)
-        self.numCrazy.setText('20')
+        self.numCrazy.setText('5')
         self.numCrazy.setValidator(onlyFloat)
         self.numCrazy.textEdited.connect(self.getCrazy)
-        self.minThreshold.setText('0.2')
+        self.minThreshold.setText('0.1')
         self.minThreshold.setValidator(onlyFloat)
         self.minThreshold.textEdited.connect(self.getThreshold)
 
@@ -245,6 +220,8 @@ class MyWindow(QtGui.QMainWindow, form_class):
         # Window is showed
         self.show()
 
+
+    ## Methods to get the configuration parameters
     def getRows(self):
         self.rowsF = float(self.rows.text())
         print self.rowsF
@@ -285,37 +262,54 @@ class MyWindow(QtGui.QMainWindow, form_class):
         self.minThresholdF = float(self.minThreshold.text())
         print self.minThresholdF
 
-    # Event of the start button
-    def button_clicked(self):
+    # Handle to update the draw space
+    def handle_draw(self):
+        print 'Señal draw recibida'
+        self.canvasSpace.update_figure()
+        self.canvasPheromone.update_figure()
+
+    def startMainWindow(self):
         # Generation of an initial solution
         self.space = Space(self.rowsF, self.colsF, self.focusF, self.numCamerasF, self.distPowerF,
-                           self.pheromPowerF, self.evaporationF, self.numAntsF, self.numCrazyF, self.minThresholdF)
+                           self.pheromPowerF, self.evaporationF, self.numAntsF, self.numCrazyF, self.minThresholdF, self.draw)
 
         self.process.setText('Process:')
         self.process.setStyleSheet('color: #1F1C1C')
 
         # It start the space image
-        canvasSpace = MyCanvasSpace(
-            self.image, self.space, parent=None, width=self.space.rows, height=self.space.cols, dpi=100)
-        self.imagesLayout.addWidget(canvasSpace)
+        self.canvasSpace = MyCanvasSpace(
+            self.image, self.space, parent=None, width=self.rowsF, height=self.colsF, dpi=100, focus=self.focusF)
+        self.imagesLayout.addWidget(self.canvasSpace)
         self.image.setFocus()
         self.setCentralWidget(self.image)
 
         # It start the pheromone image
-        canvasPheromone = MyCanvasPheromone(
-            self.image, self.space, parent=None, width=30, height=self.space.cols, dpi=100)
-        self.imagesLayout.addWidget(canvasPheromone)
+        self.canvasPheromone = MyCanvasPheromone(
+            self.image, self.space, parent=None, width=30, height=self.colsF, dpi=100)
+        self.imagesLayout.addWidget(self.canvasPheromone)
         self.pheromones.setFocus()
         self.setCentralWidget(self.pheromones)
+
+        self.draw.connect(self.handle_draw)
+
+
+
+    # Event of the start button
+    def button_clicked(self):
+
+        self.startMainWindow()
 
         # Evaluation
         self.state.setText('Estado: Processing')
         self.state.setStyleSheet('color: #AC660A')
         try:
             self.space.evaluateSpace()
-            self.space.startAlgorithm()
+            #self.space.startAlgorithm()
             self.state.setText('Estado: Finish')
             self.state.setStyleSheet('color: #069019')
-        except:
+        except Exception as inst:
+            print type(inst)     # the exception instance
+            print inst.args      # arguments stored in .args
+            print inst
             self.state.setText('Estado: Processing error')
             self.state.setStyleSheet('color: #D20101')
--- Space.py
+++ Space.py
@@ -6,47 +6,29 @@ import copy
 import numpy as np
 import matplotlib.pyplot as plt
 from PyQt4 import QtCore, QtGui, uic
+from PyQt4.QtCore import QObject, pyqtSignal
 
-# Influencia de las feromonas
-influenciaFer = 0.5
-# Influencia de la distancia respecto al espacio cubierto
-pesoFitness = 0.0
-# coeficiente de evaporación de las feromonas
-evaporacion = 0.9  # Se evapora un 10%
-# Umbral que aceptamos para obtener una solución
-umbral = 0.2
-# Número de hormigas locas
-numLocas = 20  # 5% de las hormigas son locas
-# Número de hormigas para el algoritmo
-numHormigas = 5
-# Número de cámaras por cada space
-numCamaras = 6
-# Espacio que abarca cada cámara
-focus = 35
-# Espacio máximo que abarca cada cámara
-distMax = 2 * focus
-# Filas
-rows = 50
-# Columnas
-cols = 2 * rows
+# Number of matrix columns
+cols = 100
 
+# Colors that cameras can take
 colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
 fig, ax = plt.subplots()  # note we must use plt.subplots, not plt.subplot
 
 # Main class that creates a set of cameras
-class Space:
+class Space(QObject):
     def __init__(self, *args):
-        print args
+        print 'Configuración inicial:', args
         # Space width
-        self.rows = args[0]
-        # Space long
-        self.cols = args[1]
+        self.width = args[0]
+        # Space height
+        self.height = args[1]
         # Space that each camera covers
-        self.focus = args[2]  # Se evapora un 10%
+        self.focus = args[2]
         # Number of cameras
         self.numCameras = args[3]
         # Power of distance beetween cameras
-        self.distPower = args[4]  # 5% de las ants son crazys
+        self.distPower = args[4]
         # Power of pheromones
         self.pheromPower = args[5]
         # Evaporation of pheromones
@@ -57,39 +39,38 @@ class Space:
         self.numCrazy = args[8]
         # Minimun accepted solution
         self.minThreshold = args[9]
-        # Espacio máximo que abarca cada cámara
-        distMax = 2 * focus
-        # Filas
-        rows = 50
-        # Columnas
-        cols = 100
 
-        ancho = rows
-        largo = 100
-
+        # Lists to calculate each wall
         self.wall1 = np.ones(cols)
         self.wall2 = np.ones(cols)
+
+        # Cameras of the space
         self.cameras = []
+        # Color of each camera
         self.color = []
         for i in range(self.numCameras):
             self.cameras.append(
-                (random.randint(0, 1) * rows, random.randint(0, cols)))
+                (random.randint(0, 1) * self.width, random.randint(0, cols)))
             self.color.append(random.choice(colors))
+
         self.fit = 0
-        self.cols = largo
-        self.rows = ancho
+
+        # Define a new signal called 'draw' that has no arguments
+        self.draw = args[10]
 
     # Process each camera and add the distance between them, the goal is to minimize it
     def processDistMax(self):
         dist = 0
         listCam = []
-        for a in range(numCameras):
+        for a in range(self.numCameras):
             listCam.append(self.cameras[a][1])
 
         listCam.sort()
 
-        for a in range(numCameras - 1):
+        for a in range(self.numCameras - 1):
             dist += abs(listCam[a] - listCam[a + 1])
+
+        dist = (dist * self.height / cols)/cols
 
         return dist
 
@@ -98,25 +79,25 @@ class Space:
         list1 = []
         list2 = []
 
-        for a in range(numCameras):
+        for a in range(self.numCameras):
             if self.cameras[a][0] == 0:
-                list1.append(self.cameras[a][1] * largo / cols)
+                list1.append(self.cameras[a][1] * self.height / cols)
             else:
-                list2.append(self.cameras[a][1] * largo / cols)
+                list2.append(self.cameras[a][1] * self.height / cols)
 
         list1.append(0)
-        list1.append(largo)
+        list1.append(self.height)
         list2.append(0)
-        list2.append(largo)
+        list2.append(self.height)
         list1.sort()
         list2.sort()
 
         vacio = 0
         for a in range(len(list1) - 1):
             if a == 0 or a == (len(list1) - 2):
-                dist = focus
+                dist = self.focus
             else:
-                dist = 2 * focus
+                dist = 2 * self.focus
 
             resul = list1[a + 1] - list1[a]
             if resul > dist:
@@ -124,9 +105,9 @@ class Space:
 
         for a in range(len(list2) - 1):
             if a == 0 or a == (len(list2) - 2):
-                dist = focus
+                dist = self.focus
             else:
-                dist = 2 * focus
+                dist = 2 * self.focus
 
             resul = list2[a + 1] - list2[a]
             if resul > dist:
@@ -136,23 +117,23 @@ class Space:
 
     # Method that evaluates the goodness of a space
     def evaluateSpace(self):
-        fit = self.processDistMax() * pesoFitness + \
-            self.processEmptySpace() * (1 - pesoFitness)
-        print 'fit', fit
+        fit = self.processDistMax() * self.distPower + \
+            self.processEmptySpace() * (1 - self.distPower)
         self.fit = fit
 
     # Function that places each camera in a new position based on its probability
     def processCamera(self, cam, crazy):
-        large = cam[1] * largo / cols
-        if large - focus < 0:
+        #print 'processCamera'
+        large = cam[1] * self.height / cols
+        if large - self.focus < 0:
             low = 0
         else:
-            low = large - focus
+            low = large - self.focus
 
-        if large + focus > cols:
+        if large + self.focus > cols:
             high = cols
         else:
-            high = large + focus
+            high = large + self.focus
 
         y = int(random.triangular(low, high))
 
@@ -169,7 +150,7 @@ class Space:
                         mode = self.wall2[i]
 
             print 'mode1',mode
-            mode = mode*influenciapher + cam[1]*(1-influenciapher)
+            mode = mode*pheromPower + cam[1]*(1-pheromPower)
             print 'mode2',mode
             y = int(random.triangular(low, mode, high))'''
 
@@ -177,7 +158,7 @@ class Space:
         if x == 0:
             # Cam changes from wall depending on the pheromones
             if random.uniform(0, 1) < 0.1 * self.wall2[y]:
-                x == rows
+                x == self.width
         else:
             if random.uniform(0, 1) < 0.1 * self.wall1[y]:
                 x == 0
@@ -186,9 +167,10 @@ class Space:
 
     # Algorithm that creates a space taking into account the data of the previous
     def createSpace(self, crazy):
+        #print 'New space'
         spaceNew = copy.copy(self)
-        for i in range(numCameras):
-            # You get the position of each camera
+        for i in range(self.numCameras):
+            # You get the position of each camera to build te path
             [x, y] = self.processCamera(spaceNew.cameras[i], crazy)
             # Each camera is placed in the new space, in the new position
             spaceNew.cameras[i] = [x, y]
@@ -199,11 +181,13 @@ class Space:
     # Mark the pheromones of the route
     def markPheromones(self, crazy):
         if crazy == 0:
+            print 'markPheromones'
             input = 1
         else:  # if it is a crazy ant its input is bigger
+            print 'markPheromones crazy'
             input = 5
 
-        for i in range(numCameras):
+        for i in range(self.numCameras):
             if self.cameras[i][0] == 0:
                 self.wall1[self.cameras[i][1]] += input
             else:
@@ -211,22 +195,24 @@ class Space:
 
     # Evaporates the trace of pheromones in each iteration
     def evaporatePheromones(self):
+        #print 'Evaporacion'
         for i in range(cols):
-            self.wall1[i] *= evaporacion
-            self.wall2[i] *= evaporacion
+            self.wall1[i] *= (1-self.evaporation)
+            self.wall2[i] *= (1-self.evaporation)
 
     # Ants colony algorithm that creates new ants until finding better solutions
     def ants(self):
+        print '-------------------- hormigas ------------------ '
         crazy = 0
         # For each ant
-        for i in range(numAnts):
+        for i in range(self.numAnts):
             # I mark if it is crazy
-            if random.randint(0, numcrazys) == 1:
+            if random.uniform(0, 1) < (self.numCrazy/100):
                 crazy = 1
+                print 'crazy'
             # I create a new space based in the previous
             spaceNew = self.createSpace(crazy)
 
-            crazy = 0
             # I evaluated it
             spaceNew.evaluateSpace()
 
@@ -234,10 +220,13 @@ class Space:
             if spaceNew.fit < self.fit:
                 self = spaceNew
                 self.markPheromones(crazy)
-                print spaceNew.fit
-                self.drawSpace()  # I only paint the spaces that are best
+                print '<<<<<<<<<<<< Espacio mejor', spaceNew.fit
+                # Emit draw signal
+                self.draw.emit()
+                #self.drawSpace()  # I only paint the spaces that are best
 
-            self.drawPheromones()
+            crazy = 0
+            #self.drawPheromones()
 
         # When all the ants are finished, I evaporate the pheromones
         self.evaporatePheromones()
@@ -245,12 +234,12 @@ class Space:
         return self
 
     # Auxiliary method to update the figure 1
-    def updateSpace(self):
+    '''def updateSpace(self):
         global fig, ax
         i = 1
         for cam in range(len(self.cameras)):
             ax.add_artist(plt.Circle((self.cameras[cam][1], self.cameras[cam][0]),
-                                     focus, color=self.color[cam], fill=self.color[cam], alpha=0.5, clip_on=True))
+                                     self.focus, color=self.color[cam], fill=self.color[cam], alpha=0.5, clip_on=True))
             ax.annotate("C" + str(cam),
                         [self.cameras[cam][1], self.cameras[cam][0]])
             ax.plot((self.cameras[cam][1]), (self.cameras[cam]
@@ -260,19 +249,19 @@ class Space:
         fig.gca().set_aspect('equal', 'box')
         plt.show()
         plt.pause(5)
-        ax.clear()  # which clears axes
+        ax.clear()  # which clears axes'''
 
     # Draw the cameras in a window
-    def drawSpace(self):
+    '''def drawSpace(self):
         plt.ion()
 
         ax.set_xlim((0, cols))
-        ax.set_ylim((0, rows))
+        ax.set_ylim((0, self.width))
 
-        self.updateSpace()
+        self.updateSpace()'''
 
     # Draw two graphics of pheromones
-    def drawPheromones(self):
+    '''def drawPheromones(self):
         vcols = np.zeros(cols)
         for i in range(cols):
             vcols[i] = i
@@ -296,13 +285,13 @@ class Space:
         plt.bar(vcols, self.wall1, facecolor='#1c702d', edgecolor='white')
 
         plt.subplots_adjust(hspace=0.5)
-        plt.show()
+        plt.show()'''
 
     # if __name__ == '__main__':
     def startAlgorithm(self):
 
         # I'm looking for an acceptable solution with the algorithm of the ants
-        while self.fit > umbral:
+        while self.fit > self.minThreshold:
             self = self.ants()
 
         # I show the final solution
